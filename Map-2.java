d6892cead7187eec52ba18f23c384fbd7c640825
Dmitry <38311653+OtekeDmk@users.noreply.github.com>
2018-04-13 12:57:09 +0700

    Update Map-2.java

--- Map-2.java
+++ Map-2.java
@@ -144,3 +144,57 @@ public Map<String, Boolean> wordMultiple(String[] strings) {
   }
   return map;
 }
+
+/*
+ allSwap 
+We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2 strings in the array. When a position in the array has been swapped, it no longer matches anything. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+
+
+allSwap(["ab", "ac"]) → ["ac", "ab"]
+allSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "azz", "aaa"]
+allSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "aj", "ai", "by", "bx"]
+*/
+
+public String[] allSwap(String[] strings) {
+  // № 1  
+  Map<Character, Integer> map = new HashMap<>();
+   
+   for (int i = 0; i < strings.length; i++) {
+
+         char ch = strings[i].charAt(0); 
+         Integer r = map.remove(ch);
+
+        if (r != null) {
+             int j = r; 
+             String swap = strings[j];
+            strings[j] = strings[i];
+            strings[i] = swapp;
+        } else {
+            map.put(ch, i); 
+        }
+    }
+    return strings;
+}
+ // № 2 is more memoryconsuming, then №1
+   /*
+   String[] str = new String[strings.length];    
+
+   Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+           
+            String key = strings[i].substring(0,1);
+            if (!map.containsKey(key)) {
+                map.put(key, i);
+                str[i] = strings[i];
+            } else {
+                int match = map.get(key);
+                str[i] = strings[match];
+                str[match] = strings[i];
+                map.remove(key);
+            }
+        }
+        return str;
+        */
+
+
+
